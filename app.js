d51f69804856a5737b9ad09b9034f3b6d057b826
Jonathan Fuentes <john.fuen95@gmail.com>
2023-12-22 19:12:30 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const buttons = document.querySelectorAll('.ripple');
+
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        const x = e.clientX;
+        const y = e.clientY;
+
+       const buttonTop = e.target.offsetTop;
+       const buttonLeft = e.target.offsetLeft;
+
+       const xInside = x - buttonLeft;
+       const yInside = y - buttonTop;
+
+        const span = document.createElement('span');
+        span.innerHTML = `<span class="circle" style="top: ${yInside}px; left: ${xInside}px;"></span>`;
+        e.target.appendChild(span);
+
+        setTimeout(() => span.remove(), 500)
+    })
+})
